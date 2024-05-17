97b6e55171906b6fba3cdf16202f2e0e20fedf91
Igor <sobolinskii@mail.ru>
2024-05-17 14:58:17 +0800

    realized base structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	devtools: { enabled: true },
+	css: ['~/assets/style.css'],
+
+	app: {
+		head: {
+			title: 'Learn NUXT',
+			htmlAttrs: {
+				lang: 'en',
+			},
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ hid: 'description', name: 'description', content: 'Learn NUXT' },
+				{ name: 'format-detection', content: 'telephone=no' },
+			],
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
+
+	modules: ['@nuxtjs/tailwindcss'],
+});
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>Welcome to the homepage</h1>
+		<AppAlert> This is an auto-imported component </AppAlert>
+		<AppAlert>123213123</AppAlert>
+		<ul class="qqq">
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+		</ul>
+	</div>
+</template>
+
+<style scoped>
+.qqq {
+	@apply flex gap-5 p-6 text-red-300;
+}
+</style>
